fa06844156cff4c1648b2087acf1290da0684b1c
515491847 <515491847@qq.com>
2018-08-23 11:12:30 +0800

    :construction: Work On Tree Show

--- resources/views/common/routes.blade.php
+++ resources/views/common/routes.blade.php
@@ -1,4 +1,12 @@
 <script>
     var routes = {
+    	nodes: {
+    		update: '{{ RouteUri('nodes.update') }}'
+    	},
+    	projects: {
+    		index: {
+    			get_tree: '{{ RouteUri('projects.index.get_tree') }}'
+    		}
+    	}
     };
 </script>
